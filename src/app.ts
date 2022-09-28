ac8b90f31dd50e888265e0461accaa2b636c10d7
Daniel Jankech <djankech98@gmail.com>
2022-09-28 15:21:48 +0200

    no-fe-version

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,56 @@
 import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import { Pool } from "pg";
+import axios from 'axios';
+import { setTimeout } from "timers/promises";
+
+// https://www.sreality.cz/api/en/v2/estates?category_main_cb=1&category_type_cb=1&page=1&per_page=500
+
+async function extractData (url : string) {
+    const data = (await axios.get(url, {
+        responseType: 'text',
+        validateStatus: null,
+    })).data;
+
+    return data._embedded.estates.map((item : any) => {
+        return {
+            name : item.name,
+            address: item.locality,
+            price: item.price,
+            image_url: item._links.images[0].href
+        }
+    })
+}
+
+async function savePSQL(data: any) {
+    // await pool.query('CREATE TABLE apartments_table(name text,address text,price int,image_url text)')
+    await pool.query('TRUNCATE apartments_table').then()
+    data.forEach((item : any) => {
+        pool.query('INSERT INTO apartments_table ("name", "address", "price", "image_url") values($1, $2, $3, $4)',[item.name,item.address,item.price,item.image_url])
+    })
+    /*
+    pool.query('SELECT * from apartments_table').then(resp => {
+        console.log(JSON.stringify(resp));
+    })
+     */
+}
 
 const app = express();
-dotenv.config(); //Reads .env file and makes it accessible via process.env
+dotenv.config();
 
-app.get("/test", (req: Request, res: Response, next: NextFunction) => {
-    res.send("hi");
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+    /*
+    (pool.query('SELECT $1::text as connected', ['Connection to postgres successful!'])).then((resp)=>{
+        res.send(resp.rows[0].connected);
+    });
+     */
+
+    pool.query('SELECT * from apartments_table').then(resp => {
+        res.send(resp.rows);
+    })
+
 });
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running at ${process.env.PORT}`);
@@ -25,9 +68,21 @@ const pool = new Pool({
 const connectToDB = async () => {
     try {
         await pool.connect();
+        await testConnection();
     } catch (err) {
         console.log(err);
     }
 };
-connectToDB();
+
+async function testConnection () {
+    const res = await pool.query('SELECT $1::text as connected', ['Connection to postgres successful!']);
+    console.log(res.rows[0].connected);
+}
+
+connectToDB().then(async () => {
+    const url = 'https://www.sreality.cz/api/en/v2/estates?category_main_cb=1&category_type_cb=1&page=1&per_page=500'
+    const data = await extractData(url);
+    savePSQL(data);
+});
+
 
